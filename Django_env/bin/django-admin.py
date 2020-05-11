e20549e69f2441d351f7e11b69bd1755c0d33b51
Himanshu Patel <hpatel27299@gmail.com>
2020-05-12 01:31:52 +0530

    Added Blog App

---
+++ Django_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/himanshu/Desktop/GitHub_Repos/Django-Blogging-Site/Django_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
