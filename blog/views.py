dbea1c583a9e4d634e98f6d472edfa9d682de2b8
Himanshu Patel <hpatel27299@gmail.com>
2020-05-12 02:22:22 +0530

    Added templates to Blog app

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def home(requests):
-    return HttpResponse('<h1>Blog: Home</h1>')
+posts = [
+    {
+        'author': 'Author 1',
+        'title': 'First Post',
+        'content': 'Content of Blog 1',
+        'date_published': '11 May, 2020'
+    },
+    {
+        'author': 'Author 2',
+        'title': 'Second Post',
+        'content': 'Content of Blog 2',
+        'date_published': '12 May, 2020'
+    },
+    {
+        'author': 'Author 3',
+        'title': 'Third Post',
+        'content': 'Content of Blog 3',
+        'date_published': '13 May, 2020'
+    }
+]
 
-def about(requests):
-    return HttpResponse('<h1>Blog: About</h1>')
+# Create your views here.
+def home(request):
+    context = {
+        'posts': posts,
+        'title': 'Home',
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
