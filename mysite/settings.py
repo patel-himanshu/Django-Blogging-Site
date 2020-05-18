7cbd34e27ba367acd138ee19d227832ff47e99ab
Himanshu Patel <hpatel27299@gmail.com>
2020-05-19 02:57:50 +0530

    Login and logout page completed

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,3 +128,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 # from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 # Create your views here.
@@ -10,8 +11,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Account created for {username}! You may now login.')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html') 
