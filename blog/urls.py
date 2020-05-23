1765b6394c950d537f4cb2522d1c78995bf68cc8
Himanshu Patel <hpatel27299@gmail.com>
2020-05-24 02:19:55 +0530

    Added pagination for individual author post

--- blog/urls.py
+++ blog/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, UserPostListView, PostDetailView,\
+     PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
     # path('', views.home, name='blog-home'),
     path('', PostListView.as_view(), name='blog-home'),
+    path('post/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
